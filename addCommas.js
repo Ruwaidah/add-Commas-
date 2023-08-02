3d7e5cbb6fe4598b986ef13c65c31e15e9478bbd
Ruwaidah Alfakhri <Ruwaidah.riyadh@gmail.com>
2023-08-01 19:15:53 -0500

    write the add commas function

--- addCommas.js
+++ addCommas.js
@@ -1,3 +1,25 @@
-function addCommas() {}
+function addCommas(num) {
+  const str = num.toString();
+  const strLength = str.length;
+  let newStr = [];
+  let commasPlace = strLength % 3;
+  for (let i = 0; i < strLength; i++) {
+    if (i === commasPlace && commasPlace !== 0) {
+      newStr.push(",");
+      newStr.push(str[i]);
+      commasPlace += 3;
+    } else {
+      newStr.push(str[i]);
+    }
+  }
+  return newStr.join("");
+}
 
-module.exports = addCommas;
+console.log(addCommas(1234));
+console.log(addCommas(1000000));
+console.log(addCommas(9876543210));
+console.log(addCommas(6));
+console.log(addCommas(-10));
+console.log(addCommas(-5678));
+
+// module.exports = addCommas;
